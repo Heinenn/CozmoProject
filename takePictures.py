b90a0ad20ff6d2bb6805a3dd1cfe428a1e445a7b
Johannes <jd@msv.wara.de>
2018-04-18 10:30:35 +0200

    renamed followLine to takePictures (it only takes a few sample pictures) added 10 sample pictures of different lines

---
+++ takePictures.py
@@ -0,0 +1,35 @@
+# imports
+import cv2
+import sys
+import time
+import cozmo
+import logging
+import numpy as np
+from PIL import Image
+from cozmo.util import degrees, distance_mm, speed_mmps
+
+# Logger
+log = logging.getLogger('ok.linefollow')
+
+
+def capture(robot: cozmo.robot.Robot):
+   """ Capture an image of the path and show bounding rect of the 
+      contour of the path.
+   """
+    
+   log.info('Capture image...')
+   robot.set_head_angle(cozmo.robot.MIN_HEAD_ANGLE).wait_for_completed()
+   robot.camera.image_stream_enabled = True
+   robot.set_lift_height(1.0).wait_for_completed()
+
+   for i in range(0,10): 
+      img = np.array(robot.world.latest_image.raw_image)
+      cv2.imwrite(str('test' + str(i) + '.png'), img)
+      robot.drive_straight(distance_mm(50), speed_mmps(200)).wait_for_completed()
+
+
+def cozmo_program(robot: cozmo.robot.Robot):
+    capture(robot)
+
+
+cozmo.run_program(cozmo_program, use_viewer=True, force_viewer_on_top=True)
